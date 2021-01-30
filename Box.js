02eb62aced0106eae5f9d7e48e5b880a0446d201
Arhaant <Arhaant.bhagowati@gmail.com>
2021-01-30 18:03:35 +0530

    hi, just check the code

--- Box.js
+++ Box.js
@@ -1,5 +1,9 @@
 class Box extends BaseClass {
   constructor(x, y, width, height){
+    var options ={
+      "density" : 0.5,
+      "friction" : 0.5
+    }
     super(x,y,width,height);
     this.image = loadImage("sprites/wood1.png");
   }
